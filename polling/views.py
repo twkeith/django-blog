69396a324747a7b0372a5d40afe5069426e2febe
twkeith <61339265+twkeith@users.noreply.github.com>
2020-11-08 22:54:54 -0800

    login works I guess

--- polling/views.py
+++ polling/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import Http404
 from polling.models import Poll
 
+
 def list_view(request):
     context = {'polls': Poll.objects.all()}
     return render(request, 'polling/list.html', context)
+
 
 def detail_view(request, poll_id):
     try:
