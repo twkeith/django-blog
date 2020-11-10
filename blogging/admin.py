da28f6ff0d010e59167aeecf56dfda87649d6c0e
twkeith <61339265+twkeith@users.noreply.github.com>
2020-11-09 22:59:35 -0800

    I think it works

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+
+class ThemesInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [ThemesInline]
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [ThemesInline]
+    exclude = ('posts',)
+
 # and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
