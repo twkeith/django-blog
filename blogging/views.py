7c50be5edcefe2b77850271ebc454a6fd69c3dc0
twkeith <61339265+twkeith@users.noreply.github.com>
2020-11-15 17:20:36 -0800

    Class Views done for Assignment 08

--- blogging/views.py
+++ blogging/views.py
@@ -2,6 +2,21 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.template import loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+
+class BlogListView(ListView):
+
+    template_name = 'blogging/list.html'
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=False).order_by('-published_date')
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
 
 
 def stub_view(request, *args, **kwargs):
