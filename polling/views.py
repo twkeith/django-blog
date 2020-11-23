5d6fc940509b68fee89d6e7017d79cd988d6b0f3
twkeith <61339265+twkeith@users.noreply.github.com>
2020-11-22 18:12:49 -0800

    added db

--- polling/views.py
+++ polling/views.py
@@ -4,6 +4,7 @@ from polling.models import Poll
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
+
 class PollListView(ListView):
     model = Poll
     template_name = 'polling/list.html'
